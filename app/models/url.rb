bd5806a882122f0942de0c16b6d5b7b8c2ac2326
Ricardo Rubio <rickarubio@gmail.com>
2014-05-06 14:16:28 -0700

    improves url validation

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,4 @@
 class Url < ActiveRecord::Base
   attr_accessible :address
-  validates :address, presence: true
-
+  validates :address, presence: true, format: { with: /[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix }
 end
