c7c8c08a873e9facc172fd94a19407677e885703
Ricardo Rubio <rickarubio@gmail.com>
2014-05-05 17:53:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lnkyus::Application.config.secret_token = 'e1e658cdf7b969bcd50dee44aa6d777c3fe746f2f1329bbe7b430583d1208f0372e9e4a345c0602212dd0982da67e84b69531e56fe9fdc3e552dbaf385aa297c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lnkyus::Application.config.session_store :cookie_store, key: '_lnkyus_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lnkyus::Application.config.session_store :active_record_store
